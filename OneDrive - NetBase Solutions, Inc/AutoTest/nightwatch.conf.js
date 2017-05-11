7c31ee14b54e449ddc1a242d2adccef53874baeb
Derrick Chung <tungtachung@gmail.com>
2017-05-11 15:01:47 +0800

    Create repository

---
+++ OneDrive - NetBase Solutions, Inc/AutoTest/nightwatch.conf.js
@@ -0,0 +1,27 @@
+var selenium = require('selenium-server-standalone-jar');
+var chromeDriver = require('chrome-driver-standalone');
+
+var config = {
+  "custom_commands_path": "command",
+  "page_objects_path": "page",
+  "src_folders": ["tests"],
+  "selenium": {
+    "start_process": true,
+    "server_path": selenium.path,
+    "cli_args": {
+      "webdriver.chrome.driver": chromeDriver.path
+    }
+  },
+  "test_settings": {
+    "default": {
+      "globals": {
+        "waitForConditionTimeout": 30000
+      },
+      "desiredCapabilities": {
+        "browserName": "chrome",
+      }
+    }
+  }
+}
+
+module.exports = config;
